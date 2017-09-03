cb92f67773095e8b506edb948cd4ee6bdfbb8a7a
asscriz <31381688+asscriz@users.noreply.github.com>
2017-09-03 16:41:41 +0700

    3-22

--- 3-22.c
+++ 3-22.c
@@ -5,5 +5,10 @@ int main()
 	char nick[4] = "Com";
 	char nick_1[4] = {'c','o','m','\0'};
 	char name[] = "Jirasak";
-		return 0;
+	printf("%15s%15s%15s%15s",subject,nick,nick_1,name);
+	return 0;
+	printf("%s\n",subject);
+	printf("%s\n",nick);
+	printf("%s\n",nick_1);
+	printf("%s\n",name);
 }
